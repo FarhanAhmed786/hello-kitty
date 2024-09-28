5c805c53e387c4dc6fa03466d47aa58761626328
stackgen-automation <automation@stackgen.com>
2024-09-28 10:01:52 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,4 +1,10 @@
 
+output "aws_lambda_hello_kitty_function_farhan_ahmed_function_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+  sensitive = false
+}
+
+
 output "aws_lambda_hello_kitty_function_farhan_ahmed_function_name" {
   value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
   sensitive = false
@@ -13,12 +19,6 @@ output "aws_lambda_hello_kitty_function_farhan_ahmed_function_url" {
 
 output "aws_lambda_hello_kitty_function_farhan_ahmed_invoke_arn" {
   value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_function_farhan_ahmed_function_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
   sensitive = false
 }
 
